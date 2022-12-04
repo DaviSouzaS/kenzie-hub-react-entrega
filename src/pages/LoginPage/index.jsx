e26f2fc6be53dc2bc9b96c9c3f855004bddd0199
davi <daavisouza.santana@gmail.com>
2022-12-04 12:26:57 -0300

    feat: create anchors

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,15 +1,7 @@
 import { LoginForm } from "../../components/LoginForm"
-import { useNavigate } from "react-router-dom"
-import { Button } from "../../components/Button"
 import logo from "../../assets/logo.svg"
 
 export function LoginPage ({setUser}) {
-
-    const navigate = useNavigate()
-
-    function redirectToRegister () {
-        navigate("/registerPage")
-    }
 
     return (
         <div>
@@ -19,10 +11,9 @@ export function LoginPage ({setUser}) {
                     <h1>Login</h1>
                     <LoginForm setUser = {setUser}/>
                     <p>Ainda não possui uma conta?</p>
-                    <Button type={"button"} name = {"Cadastre-se"} redirect = {redirectToRegister}/>
+                    <a href="/registerPage">Cadastre-se</a>
                 </div>
             </div>
         </div>
-        
     )
 }
