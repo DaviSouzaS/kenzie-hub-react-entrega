becf1e98eec425ad39836fb823363f92879557ed
davi <daavisouza.santana@gmail.com>
2022-12-12 12:26:02 -0300

    feat: completeDeleteFunction

--- src/components/TechList/index.jsx
+++ src/components/TechList/index.jsx
@@ -1,15 +1,22 @@
 import { StyledTech } from "./style"
+import { TechContext } from "../../contexts/TechContext"
+import { useContext } from "react"
 import trashIcon from "../../assets/trash-icon.svg"
+import { UserContext } from "../../contexts/UserContext"
+import spinner from "../../assets/spinner.svg"
 
 export function TechItem ({tech}) {
+
+    const { deleteTech } = useContext (TechContext)
+    const { loading } = useContext (UserContext)
 
     return (
         <StyledTech className="display-flex justfy-content-between align-item"> 
             <h2>{tech.title}</h2> 
             <span className="display-flex gap-25">
                 <p>{tech.status}</p>
-                <button>
-                    <img src={trashIcon} alt="delete-button" />
+                <button onClick={() => deleteTech(tech.id)}>
+                <img src={trashIcon} alt="delete-button" />
                 </button>
             </span>
         </StyledTech>
