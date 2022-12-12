bf98830af222b1bfb9c0c1ebe3106a03f604e16a
davi <daavisouza.santana@gmail.com>
2022-12-12 05:28:46 -0300

    feat: createAutoLogin

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,51 +1,53 @@
 import { createContext, useState } from "react";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
-import { request } from "../services/api"
+import { request } from "../services/api";
+import { useEffect } from "react";
 
 export const UserContext = createContext({});
 
 export function UserProvider({ children }) {
-  const [user, setUser] = useState([]);
+  const [user, setUser] = useState(null);
   const [showPassword, setShowPassword] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [userInfos, setUserInfos] = useState([])
+  const [loadingPage, setLoadingPage] = useState(true);
+  const [userInfos, setUserInfos] = useState([]);
 
   const navigate = useNavigate();
 
-  async function login (data) {
+  async function login(data) {
     try {
-        setLoading(true)
-        const response = await request.post("/sessions", data) 
-        setUser(response.data.user) 
-        window.localStorage.clear()
-        window.localStorage.setItem("@TOKEN", response.data.token) 
-        window.localStorage.setItem("@USERID", response.data.user.id) 
-        setLoading(false)
-        toastSuccessLogin()
-        setTimeout(() => {
-            navigate("/dashboard")
-        }, 2000);
-    } catch (error){
-        setLoading(false)
-        toastError()
+      setLoading(true);
+      const response = await request.post("/sessions", data);
+      setUser(response.data.user);
+      window.localStorage.clear();
+      window.localStorage.setItem("@TOKEN", response.data.token);
+      window.localStorage.setItem("@USERID", response.data.user.id);
+      setLoading(false);
+      toastSuccessLogin();
+      setTimeout(() => {
+        navigate("/dashboard");
+      }, 2000);
+    } catch (error) {
+      setLoading(false);
+      toastError();
     }
   }
 
-  async function registerUser (data) {
+  async function registerUser(data) {
     try {
-        const response = await request.post("/users", data)
-        toastSuccessRegister()
-        setTimeout(() => {
-            navigate("/")
-        }, 2000);
-    } catch (error) { 
-        toastError()
+      const response = await request.post("/users", data);
+      toastSuccessRegister();
+      setTimeout(() => {
+        navigate("/");
+      }, 2000);
+    } catch (error) {
+      toastError();
     }
   }
 
   function showPass() {
-    setShowPassword(!showPassword)
+    setShowPassword(!showPassword);
   }
 
   function toastError() {
@@ -74,46 +76,65 @@ export function UserProvider({ children }) {
     });
   }
 
-  function toastSuccessRegister () {
+  function toastSuccessRegister() {
     toast.success("Conta criada com sucesso!", {
-        position: "top-right",
-        autoClose: 1000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "dark",
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
     });
   }
 
-    async function getUserInfos () {
-        const token = localStorage.getItem("@TOKEN")
+  async function getUserInfos() {
+    const token = localStorage.getItem("@TOKEN");
 
-        restriction(token)
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
 
-        const config = {
-           headers: { Authorization : `Bearer ${token}`}
-        }
+    try {
+      const response = await request.get("/profile", config);
+      setUserInfos(response.data);
+    } catch (error) {}
+  }
 
-        try {
-            const response = await request.get("/profile", config)
-            setUserInfos(response.data)
-        } catch (error) {
-        }
+  useEffect(() => {
+
+    async function verifyingToken () {
+
+    const token = localStorage.getItem("@TOKEN");
+
+    if (!token) {
+      setLoadingPage(false)
+      return
     }
 
-    function restriction (token) {
-    if (token === null) {
-        navigate("/")
-    }
-    }
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
 
-   function logout () {
-    setUser([])
-    window.localStorage.clear()
-    navigate("/")
+    try {
+      const response = await request.get("/profile", config);
+      setUser(response.data);
+    } catch (error) {
+      window.localStorage.clear();
+    } finally {
+      setLoadingPage(false)
     }
+    }
+    
+    verifyingToken()
+  }, []);
+
+  function logout() {
+    setUser(null);
+    window.localStorage.clear();
+    navigate("/");
+  }
 
   return (
     <UserContext.Provider
@@ -122,13 +143,15 @@ export function UserProvider({ children }) {
         showPass,
         loading,
         setLoading,
-        userInfos, 
+        loadingPage,
+        userInfos,
         getUserInfos,
-        restriction,
         login,
         registerUser,
-        logout
-      }}>
+        logout,
+        user
+      }}
+    >
       {children}
     </UserContext.Provider>
   );
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -2,20 +2,22 @@ import { Header } from "../../components/Header"
 import { StyledDahsboard } from "./style"
 import { useContext, useEffect } from "react"
 import { UserContext } from "../../contexts/UserContext"
+import { Navigate } from "react-router-dom"
 
 export  function Dashboard () {
 
-    const {userInfos, getUserInfos, restriction} = useContext (UserContext)
+    const {userInfos, getUserInfos, user, loadingPage} = useContext (UserContext)
 
     useEffect(() => {
         getUserInfos()
     }, [])
 
-    restriction()
+    if (loadingPage) {
+        return null
+    }
     
-    return (
-        
-        <StyledDahsboard>
+    return user ?
+       <StyledDahsboard>
           <Header/>
 
           <div className=" user-infos display-flex align-item">
@@ -30,5 +32,7 @@ export  function Dashboard () {
                 <p className="waring-2">Nossa aplicação está em desenvolvimento, em breve teremos novidades</p>
             </main>
         </StyledDahsboard>
-    )
+        : 
+        <Navigate to = '/'/>
+    
 }
